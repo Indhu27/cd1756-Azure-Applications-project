ce616838624be350cb85f9d0fdede4ce968fe925
Indhu27 <105510890+Indhu27@users.noreply.github.com>
2025-11-18 07:04:19 +0530

    Added OAuth authentication and logging functionality

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,6 +2,8 @@
 The flask application package.
 """
 import logging
+from logging.handlers import RotatingFileHandler
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -10,10 +12,28 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s'))
+app.logger.addHandler(stream_handler)
+
+app.logger.setLevel(logging.INFO)
+app.logger.info('CMS Application startup')
 
 import FlaskWebProject.views
